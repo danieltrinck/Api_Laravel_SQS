afae25b53e4755b49f79113d912021bcedaa3aa3
delgps <delgps@yahoo.com.br>
2024-05-19 13:55:32 -0300

    arquivos do laravel

--- www/database/seeders/DatabaseSeeder.php
+++ www/database/seeders/DatabaseSeeder.php
@@ -4,19 +4,25 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
     /**
      * Seed the application's database.
+     *
+     * @return void
      */
-    public function run(): void
+    public function run()
     {
         // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        \App\Models\User::factory()->create([
+           'name'              => 'Irroba',
+           'email'             => 'api@irroba.com',
+           'password'          => Hash::make('irroba'),
+           'email_verified_at' => now(),
+           'remember_token'    => Str::random(10),
+        ]);
     }
 }
